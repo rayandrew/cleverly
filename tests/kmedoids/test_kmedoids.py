1283cdadd173d5c9133672d9c5c4571d94c57165
girvandip <girvandip@gmail.com>
2018-10-17 19:21:20 +0700

    Added KMedoids class and tests

---
+++ tests/kmedoids/test_kmedoids.py
@@ -0,0 +1,30 @@
+import unittest
+
+from numpy import ndarray, testing
+from sklearn.externals import joblib
+
+from ..context import iris_data, iris_target, check_model_exist
+from clustry.kmedoids import KMedoids
+
+
+class KMedoidsTestSuite(unittest.TestCase):
+    """KMedoids test cases."""
+
+    @classmethod
+    def setUpClass(self):
+        print(1)
+        self.filename = './tests/models/kmedoids.model'
+        if check_model_exist(self.filename):
+            self.kmedoids = joblib.load(self.filename)
+        else:
+            self.kmedoids = KMedoids(
+                n_clusters=3)
+            self.kmedoids.fit_predict(iris_data)
+            joblib.dump(self.kmedoids, self.filename)
+
+    def test_kmeans_return_labels_with_type_numpy_array(self):
+        self.assertIsInstance(self.kmedoids.labels_, ndarray)
+
+
+if __name__ == '__main__':
+    unittest.main()
